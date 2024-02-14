007cc5ebed77d226a56a5e6c361f6c5deee5df33
Calma Industry <51451578+CalmaIndustry@users.noreply.github.com>
2024-02-14 18:38:45 +0100

    Create s42.tf

---
+++ sandboxtestcalma/terraform/s42.tf
@@ -0,0 +1,6 @@
+data "aws_caller_identity" "current" {}
+
+resource "aws_s3_bucket" "sandbox_bucket22" {
+  bucket = "aft-sandbox-22-${data.aws_caller_identity.current.account_id}"
+  acl    = "private"
+}
